71aaef0d30796345b282daea084639139a63c368
Yasmim <yasmimdecastroribeiro@gmail.com>
2025-02-24 11:06:02 -0300

    Finaliza programa Minhas 5 Frutas Favoritas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(" --- Minhas Frutas Preferidas ---" ); 
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Purple;
+Console.BackgroundColor = ConsoleColor.LightPurple;
+Console.WriteLine(" 1. Uva ");
+Console.ResetColor();
+Console.WriteLine(" 2. Manga ");
+Console.WriteLine(" 3. Cereja");
+Console.WriteLine( "4. Melancia ");
+Console.WriteLine( " 5. Melão ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(" 2. Manga ");
+Console.ResetColor();
